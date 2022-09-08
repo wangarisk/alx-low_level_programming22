43168e3ba283389a20e97fe2ea1a202561f76a73
wangarisk <w.skinuthia@gmail.com>
2022-09-08 15:51:56 -0700

    obey betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main - Entry point
+ *
+ * Description: print statement
+ *
+ * Return: Always 0 (Success)
+ */
+
+int  main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
