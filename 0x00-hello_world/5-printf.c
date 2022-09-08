911891b5a16a41ff7c73d42c4fe7b10ed3313e18
wangarisk <w.skinuthia@gmail.com>
2022-09-08 15:34:26 -0700

    correct error

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/* print statement */
+/**
+ * main - Entry point
+ *
+ * Description: print a statement
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n") ;
-	return (0) ;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
