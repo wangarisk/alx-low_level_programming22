a8990292f29e2c75d0425fa7b1b2c8cde3d54516
wangarisk <w.skinuthia@gmail.com>
2022-09-22 13:57:02 -0700

    task 0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, desr_len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_len++ 
+		dest_len++;
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
