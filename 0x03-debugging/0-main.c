583db8161116d1fda07a96771b5ddc5751d698bd
wangarisk <w.skinuthia@gmail.com>
2022-09-22 17:42:51 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints positive or negative integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+
+	positive_ or_ negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_ remaining_ days(int month, int day, int year);
+
+int convert_ day(int month, int day);
+
+#endif
